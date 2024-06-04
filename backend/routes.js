afd291ad6e6b397508fd6b112680c53779af29d5
Anthony <anthonyb@versa-networks.com>
2024-06-04 02:50:46 -0400

    fix: remove unused routes

--- backend/routes.js
+++ backend/routes.js
@@ -11,31 +11,6 @@ const __dirname = path.dirname(__filename);
 
 const router = express.Router();
 
-// async function streamToString(stream) {
-//     // lets have a ReadableStream as a stream variable
-//     const chunks = [];
-
-//     for await (const chunk of stream) {
-//         chunks.push(Buffer.from(chunk));
-//     }
-
-//     return Buffer.concat(chunks).toString("utf-8");
-// }
-
-router.post('/generate-summary', async (req, res) => {
-    const { decision } = req.body;
-    //const summary = await generateSummary(decision);
-    res.status(200).send({summary: 'summary'});
-});
-
-// router.post('/upload-pdf', async (req, res) => {
-//     const { pdf } = req.body;
-    
-//     res.status(200).send({text: streamToString(pdf)});
-    
-// });
-
-
 const upload = multer({ dest: 'uploads/' });
 
 // POST route to handle PDF upload and conversion to text
